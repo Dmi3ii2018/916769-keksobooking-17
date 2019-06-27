2494c0d6cabc4420557f0b10d904e5987e5cc0d0
Dmitrii Alekseev <dalekseev2017@gmail.com>
2019-06-28 02:14:53 +0500

    Подключил сервер

--- js/data.js
+++ js/data.js
@@ -3,9 +3,7 @@
 (function () {
   var NEIGHBORS_NUMBER = 8;
 
-  // блок для вставки пинов
   var mapPinsContainer = document.querySelector('.map__pins');
-  // шаблон пина
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
   // новый элемент
@@ -21,9 +19,7 @@
     return mapPin;
   };
 
-  // добавление элемента на страницу
   window.onSuccess = function (neighbor) {
-    // var neighbors = createNeighborPin();
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < NEIGHBORS_NUMBER; i++) {
       fragment.appendChild(getPin(neighbor[i]));
