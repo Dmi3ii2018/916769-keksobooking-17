de12a11bf73984745b28695c907b65985a748a80
Dmitrii Alekseev <dalekseev2017@gmail.com>
2019-07-09 00:52:39 +0500

    Использовал объект для логики новых пинов

--- js/render-pins.js
+++ js/render-pins.js
@@ -1,30 +1,40 @@
 'use strict';
 
 (function () {
+  var SHOW_NEIGHBORS_NUMBER = 5;
   var mapPinsContainer = document.querySelector('.map__pins');
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
   window.sortNeighbors = [];
 
+  var renderPin = function (neighbor) {
+    var mapPin = pinTemplate.cloneNode(true);
+    var mapPinImage = mapPin.querySelector('img');
+
+    mapPin.style.left = neighbor.location.x + 'px';
+    mapPin.style.top = neighbor.location.y + 'px';
+
+    mapPinImage.src = neighbor.author.avatar;
+    mapPinImage.alt = neighbor.offer.type;
+
+    return mapPin;
+  };
+
   // новый элемент
-  window.getPin = function (neighbor) {
-    var takeNumber = neighbor.length > 5 ? 5 : neighbor.length;
+  window.getPin = function (data) {
     mapPinsContainer.innerHTML = '';
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < takeNumber; i++) {
-      var mapPin = pinTemplate.cloneNode(true);
+    data.slice(0, SHOW_NEIGHBORS_NUMBER).map(function (it) {
+      return new window.Neighbor(it);
+    }).forEach(function (neighbor) {
+      fragment.appendChild(renderPin(neighbor));
+    });
 
-      mapPin.style.left = neighbor[i].location.x + 'px';
-      mapPin.style.top = neighbor[i].location.y + 'px';
-
-      mapPin.querySelector('img').src = neighbor[i].author.avatar;
-      mapPin.querySelector('img').alt = neighbor[i].offer.type;
-
-      fragment.appendChild(mapPin);
-    }
     mapPinsContainer.appendChild(window.mapPinMain);
     mapPinsContainer.appendChild(fragment);
+
+    return fragment;
   };
 
   window.onSuccess = function (neighbor) {
